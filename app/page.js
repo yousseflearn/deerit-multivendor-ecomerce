c91540f8d3c0f73141ec33e0851cf9a38c3ff332
yousseflearn <youssefeasylearn@gmail.com>
2025-02-24 09:17:27 -0800

    Setup of Project

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center flex-col min-h-screen">
+      <h2 className="text-4xl">Welcome To DeerIT E-commerce</h2>
+    </div>
+  );
+}
